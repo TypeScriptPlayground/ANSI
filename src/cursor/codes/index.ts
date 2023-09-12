76ba47c281b4e55a9431bd9c13e99d0903f30eb0
Mqxx <62719703+Mqxx@users.noreply.github.com>
2023-09-12 15:29:08 +0200

    Renamed folder: `move` -> `codes`

---
+++ src/cursor/codes/index.ts
@@ -0,0 +1,27 @@
+import column from "./column.ts";
+import downScroll from "./down_scroll.ts";
+import downStart from "./down_start.ts";
+import down from "./down.ts";
+import home from "./home.ts";
+import left from "./left.ts";
+import position from "./position.ts";
+import right from "./right.ts";
+import to from "./to.ts";
+import upScroll from "./up_scroll.ts";
+import upStart from "./up_start.ts";
+import up from "./up.ts";
+
+export const codes = {
+    home,
+    to,
+    up,
+    upStart,
+    upScroll,
+    down,
+    downStart,
+    downScroll,
+    left,
+    right,
+    column,
+    position
+}
